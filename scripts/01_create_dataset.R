e891c6034e326b332dff91c39f1dd5a7d4d623db
Matthew Loop <matthew_loop@auburn.edu>
2024-01-25 16:29:29 -0600

    First commit of analysis file
    Cleaned up variables with multiple levels and added variable labels

---
+++ scripts/01_create_dataset.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+library(googlesheets4)
+library(sjlabelled)
+
+df <- read_sheet("https://docs.google.com/spreadsheets/d/1DBve4JLWO-tCRHOF8aTBYVG_T5ndqQikje37zUBLCMU/edit#gid=0")
+
+df %>% Hmisc::describe()
+
+df_clean <- df %>%
+  select(-name_closest_ed, 
+         -`...16`,
+         -source_municipality_population,
+         -municipality_population_year) %>%
+  mutate(acc_chest_pain_accreditation_or_certification = if_else(acc_chest_pain_accreditation_or_certification == "no?", "no", acc_chest_pain_accreditation_or_certification)) %>%
+  var_labels(
+    school = "College/University",
+    municipality_name = "Municipality",
+    municipality_county = "County",
+    municipality_state = "State",
+    conference = "2023 Football Conference",
+    football_stadium_capacity = "Football stadium capacity",
+    municipality_population = "Population size",
+    distance_nearest_emergency_department_miles = "Distance to nearest ED (miles)",
+    academic_hospital_in_municipality = "Academic hospital present in municipality",
+    is_closest_ed_free_standing = "Closest ED is free standing",
+    acc_chest_pain_accreditation_or_certification = "Closest ED has ACC Chest Pain Accreditation or Certification",
+    closest_ed_number_beds = "Number of beds in closest ED"
+  )
+
+df_clean
+
+write_rds(df_clean, "../data/clean/01_clean_dataset.rds")
