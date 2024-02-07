c0180f961074e469bdc62d8e606c5ed50c8c0879
averynobles <157645822+averynobles@users.noreply.github.com>
2024-02-07 15:01:04 -0600

    Update 02_create_scatterplots

--- scripts/02_create_scatterplots.R
+++ scripts/02_create_scatterplots.R
@@ -37,20 +37,26 @@ ed %>%
   theme_minimal() +
   guides(color = guide_legend(override.aes = list(shape = c(16))))
 
-ed %>%
+f <- ed %>%
   ggplot(aes(x = log2(distance_nearest_emergency_department_miles), y = football_stadium_capacity)) +
   geom_point(size = 3, color = auburn_blue) +
   geom_label_repel(aes(label = school), max.overlaps = 100, size = 2, box.padding = 1, color = auburn_blue) +
-  labs(x = 'Distance to Nearest Emergency Department', y = 'Football Stadium Capacity',
-       title = 'Scatterplot of Distance to the Nearest Emergency Department vs Football Stadium Capacity') +
+  labs(x = 'log2(Distance to Nearest Emergency Department)', y = 'Football Stadium Capacity',
+       title = 'Scatterplot of Football Stadium Capacity vs Distance to the Nearest Emergency Department') +
   theme_minimal()
 
 p <- ed %>%
   ggplot(aes(x = log2(closest_ed_number_beds), y = football_stadium_capacity)) +
   geom_point(size = 3, color = auburn_blue) +
-  geom_label_repel(aes(label = school), max.overlaps = 100, size = 2, box.padding = 1, color = auburn_blue) +
-  labs(x = 'Closest Emergency Department Number of Beds', y = 'Football Stadium Capacity',
-       title = 'Scatterplot of the Number of Beds in the Closest Emergency Department vs Football Stadium Capacity') +
+  geom_label_repel(aes(label = school), max.overlaps = 100, size = 2, box.padding = 1, color = auburn_blue, label.r = 0, alpha = 0.5) +
+  labs(x = 'log2(Closest Emergency Department Number of Beds)', y = 'Football Stadium Capacity',
+       title = 'Scatterplot of Football Stadium Capacity vs the Number of Beds in the Closest Emergency Department') +
   theme_minimal()
 
-ggsave('Distance to Nearest ED vs Football Stadium Capacity.pdf', p, width = 14, height = 6, units = 'in')
+ggsave('Distance to Nearest ED vs Football Stadium Capacity.pdf',p, width = 14, height = 6, units = 'in')
+
+warnings()
+
+ggsave('Distance to Nearest ED vs. Football Stadium Capacity.pdf',f, width = 14, height = 6, units = 'in')
+
+p
